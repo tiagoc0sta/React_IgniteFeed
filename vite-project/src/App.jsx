524c2760e767b7f131e70cfbdc693f90a4f9dc53
Tiago Costa <tiagodpv@gmail.com>
2022-08-20 03:12:52 -0400

    add header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,16 @@
+import { Header } from './components/Header';
 import {Post} from './Post';
+
+import './global.css';
 
 export function App() {
   
 
   return (
     <div>
+
+      <Header/>
+
       <Post 
         author="Tiago Costa" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellat beatae nemo voluptate! Aperiam temporibus iusto cupiditate voluptate quod esse porro culpa, dignissimos explicabo repellendus nemo maiores illo vero. Incidunt!"
