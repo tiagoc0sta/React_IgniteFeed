120a4b957af6b83771e42a9b1e3bafc3f69179df
Tiago Costa <tiagodpv@gmail.com>
2022-08-20 01:36:34 -0400

    error npm run dev

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import {Post} from './Post';
+
+export function App() {
+  
+
+  return (
+    <div>
+      <Post 
+        author="Tiago Costa" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellat beatae nemo voluptate! Aperiam temporibus iusto cupiditate voluptate quod esse porro culpa, dignissimos explicabo repellendus nemo maiores illo vero. Incidunt!"
+        />      
+    </div>
+    
+  )
+}
+
+
