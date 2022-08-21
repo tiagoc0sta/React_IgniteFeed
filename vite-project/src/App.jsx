a02ae8bf228fca4530e390838b9150c01e7ec539
Tiago Costa <tiagodpv@gmail.com>
2022-08-21 02:05:10 -0400

    add post screen

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,11 @@
 import { Header } from './components/Header';
-import {Post} from './Post';
+import {Post} from './components/Post';
+import {Sidebar} from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
+
 
 export function App() {
   
@@ -11,10 +15,20 @@ export function App() {
 
       <Header/>
 
-      <Post 
-        author="Tiago Costa" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellat beatae nemo voluptate! Aperiam temporibus iusto cupiditate voluptate quod esse porro culpa, dignissimos explicabo repellendus nemo maiores illo vero. Incidunt!"
-        />      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="Tiago Costa" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellat beatae nemo voluptate! Aperiam temporibus iusto cupiditate voluptate quod esse porro culpa, dignissimos explicabo repellendus nemo maiores illo vero. Incidunt!"
+          />   
+
+          <Post 
+          author="TFC" 
+          content="Nulla repellat beatae nemo voluptate! Aperiam temporibus iusto cupiditate voluptate quod esse porro culpa, dignissimos explicabo repellendus nemo maiores illo vero. Incidunt!"
+          />                            
+        </main>
+      </div>
     </div>
     
   )
---
+++ vite-project/src/components/Post.jsx
@@ -0,0 +1,39 @@
+import styles from './Post.module.css';
+
+export function Post(){
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src='https://github.com/tiagoc0sta.png'/>
+          <div className={styles.authorInfo}>
+            <strong>Tiago Costa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time 
+          title="21-Ago at 08:13" 
+          dateTime="2022-08-22 08:13:30">Published 1 hour ago
+        </time>
+
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉{' '} <a href="">jane.design/doctorcare</a></p>
+
+        <p>
+          <a href="">#novoprojeto </a>{' '}
+          <a href="">#nlw </a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>  
+
+    
+    </article>
+  )
+}
