6f7023ea781f9f5ff719dbee49a6bc53a6aa786c
Tiago Costa <tiagodpv@gmail.com>
2022-08-22 20:32:01 -0400

    add useState

--- vite-project/src/components/Post.jsx
+++ vite-project/src/components/Post.jsx
@@ -1,32 +1,61 @@
+import {format, formatDistanceToNow} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post(props){
-  console.log(props);
+export function Post({author, publishedAt, content}){
+  const [comments, setComments] = useState ([
+    1,
+    2,
+  ])
+
+  const publishedDateFormatted = format(publishedAt, "d  LLLL 'at' HH:mm'h'");
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+    tolocale: ptBR,
+    addSuffix: true,
+  })
+
+  function handleCreateNewComment(comment) {
+    event.preventDefault()
+
+    setComments([...comments, comments.length +1]);
+
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/tiagoc0sta.png'/>
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Tiago Costa</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time 
-          title="21-Ago at 08:13" 
-          dateTime="2022-08-22 08:13:30">Published 1 hour ago
+          title={publishedDateFormatted}  dateTime={publishedAt.toISOString()}>
+            {publishedDateRelativeToNow}
         </time>
 
       </header>
 
       <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return<p>{line.content}</p>
+          }else if (line.type === 'link'){
+            return<p><a href="#">{line.content}</a></p>
+          }
+        })}
         
       </div>  
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Drop a feedback</strong>
 
         <textarea 
@@ -39,9 +68,9 @@ export function Post(props){
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        {comments.map(comment =>{
+          return <Comment />
+        })}
       </div>    
     </article>
   )
